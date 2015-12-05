37ebec4e181873c8153573fcd68c943b4c771226
Christopher Bero <bigbero@gmail.com>
2015-12-05 11:30:20 -0600

    committing the qt debug program

--- Qt/sliderDebug/mainwindow.h
+++ Qt/sliderDebug/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+//#include <QtSerialPort/QSerialPortInfo>
+#include <QDebug>
+#include <QString>
+#include <QByteArray>
+#include <QPointer>
+#include <QFont>
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +21,15 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    QPointer<QSerialPort> usbPort;
+
+ public slots:
+    void doRead();
 
 private:
     Ui::MainWindow *ui;
+    //QSerialPortInfo portInfo;
+
 };
 
 #endif // MAINWINDOW_H
