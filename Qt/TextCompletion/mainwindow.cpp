3feb293a45ed1e894f6290286107c68ae8546c6d
Mike Baldwin <mikegb562@gmail.com>
2015-12-07 13:12:46 -0600

    Pushing text completion

---
+++ Qt/TextCompletion/mainwindow.cpp
@@ -0,0 +1,105 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QThread *thread = new QThread();
+    SQLWorker *myWorker = new SQLWorker;
+    myWorker->moveToThread(thread);
+    thread->start();
+    connect(ui->lineEdit, SIGNAL (textChanged(QString)), myWorker, SLOT (doWork(QString)));
+    connect(myWorker, SIGNAL(finished(QString*)), this, SLOT(print(QString*)));
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(press0()));
+    connect(ui->pushButton_2, SIGNAL(clicked()), this, SLOT(press1()));
+    connect(ui->pushButton_3, SIGNAL(clicked()), this, SLOT(press2()));
+    connect(ui->pushButton_4, SIGNAL(clicked()), this, SLOT(press3()));
+    connect(ui->pushButton_5, SIGNAL(clicked()), this, SLOT(press4()));
+    connect(ui->pushButton_6, SIGNAL(clicked()), ui->lineEdit, SLOT(clear()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::print(QString *argv)
+{
+    ui->pushButton->setText(argv[0]);
+    ui->pushButton_2->setText(argv[1]);
+    ui->pushButton_3->setText(argv[2]);
+    ui->pushButton_4->setText(argv[3]);
+    ui->pushButton_5->setText(argv[4]);
+}
+
+SQLWorker::SQLWorker()
+{
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("C:\\test.db");
+}
+
+SQLWorker::~SQLWorker(){}
+
+void SQLWorker::doWork(QString arg)
+{
+    if (!db.open())
+        QMessageBox::warning(0, "Failed to open", "Failed to open");
+    QString *out = new QString[5];
+    QString sql = "SELECT WORD FROM dictionary WHERE WORD LIKE '" + arg + "%' ORDER BY COUNT DESC LIMIT 5;";
+    QSqlQuery query(db);
+    query.exec(sql);
+    if (!query.isActive())
+        QMessageBox::warning(0, "Bad Query", QString("Bad Query, It's inactive:%1").arg(query.lastError().text()));
+    else
+    {
+        for (int i = 0; i < 5; i = i + 1)
+        {
+            query.next();
+            out[i] = query.value(0).toString();
+        }
+        finished(out);
+    }
+    db.close();
+}
+
+void MainWindow::press0()
+{
+    ui->lineEdit->setText(ui->pushButton->text());
+}
+
+void MainWindow::press1()
+{
+    ui->lineEdit->setText(ui->pushButton_2->text());
+}
+
+void MainWindow::press2()
+{
+    ui->lineEdit->setText(ui->pushButton_3->text());
+}
+
+void MainWindow::press3()
+{
+    ui->lineEdit->setText(ui->pushButton_4->text());
+}
+
+void MainWindow::press4()
+{
+    ui->lineEdit->setText(ui->pushButton_5->text());
+}
+
+void MainWindow::pressE()
+{
+    ui->lineEdit->clear();
+}
+
+bool MainWindow::eventFilter(QObject *target, QEvent *event)
+{
+    if (event->type() = QEvent::KeyPress)
+    {
+        QKeyEvent *k = (QKeyEvent*)event;
+
+    }
+    return QObject::event(event);
+}
---
+++ Qt/TextCompletion/mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSql/QtSql>
+#include <QMessageBox>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+private slots:
+    void print(QString *argv);
+    void press0();
+    void press1();
+    void press2();
+    void press3();
+    void press4();
+    void pressE();
+};
+
+class SQLWorker : public QObject
+{
+    Q_OBJECT
+public:
+    SQLWorker();
+    ~SQLWorker();
+public slots:
+    void doWork(QString arg);
+signals:
+    void finished(QString *argv);
+private:
+    QSqlDatabase db;
+};
+#endif // MAINWINDOW_H
